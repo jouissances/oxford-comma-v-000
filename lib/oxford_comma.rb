0e376d061ea6fbc1bc6c74830f45e8c2ce65ab7b
Janice Darikho <janicedarikho@gmail.com>
2018-05-19 08:19:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join("")
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size == 3
+  elsif array.size >= 3
     second_part = array.pop()
     first_part = array.join(', ')
     first_part.concat(", and #{second_part}")
